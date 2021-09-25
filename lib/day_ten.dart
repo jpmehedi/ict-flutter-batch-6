b43af32cac1d15eda3cad465fa5d331a0889834a
Mehedi Hasan <mehedi@augnitive.com>
2021-09-25 15:17:45 +0600

    Stack, Positioned, Drawer

--- lib/day_ten.dart
+++ lib/day_ten.dart
@@ -7,8 +7,70 @@ class DayTen extends StatelessWidget {
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(title: Text("ListView & ListTile"),),
+      drawer: Drawer(
+        child: Column(
+          children: [
+            Stack(
+              children: [
+                Image.asset("assets/background.jpg"),
+                Positioned(
+                  top: 20,
+                  left: 20,
+                  child: CircleAvatar(
+                    radius: 28,
+                    backgroundImage: AssetImage("assets/poor_man.png"),
+                  ),
+                ),
 
+                Positioned(
+                  left: 20,
+                  top: 80,
+                  child: Text(
+                    "Mehedi Hasan",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24
+                    ),
+                  ),
+                ),
+                
+                Positioned(
+                  left: 20,
+                  top: 110,
+                  child: Text(
+                    "mehedihasandinfrom@gamil.com",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 14
+                    ),
+                  ),
+                )
+              ],
+            ),
 
+            ListTile(
+              onTap: (){},
+              leading: Icon(Icons.music_note),
+              title: Text("Music"),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Icon(Icons.music_note),
+              title: Text("Music"),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Icon(Icons.music_note),
+              title: Text("Music"),
+            ),
+            ListTile(
+              onTap: (){},
+              leading: Icon(Icons.music_note),
+              title: Text("Music"),
+            )
+          ],
+        ),
+      ),
       body:ListView.separated(
         separatorBuilder: (BuildContext context, int index){
           return Divider(
