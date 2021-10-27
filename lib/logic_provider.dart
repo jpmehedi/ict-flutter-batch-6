d26bb0269fbe63ddf42e58978ca7694983c75b06
Mehedi Hasan <mehedi@augnitive.com>
2021-10-27 16:03:27 +0600

    PopupMenuItem

---
+++ lib/logic_provider.dart
@@ -0,0 +1,14 @@
+
+import 'package:flutter/material.dart';
+
+class LogicScreen extends ChangeNotifier{
+
+    int _count = 0;
+    int get count => _count;
+
+    void increment() {
+      _count ++;
+      notifyListeners();
+    }
+
+}
