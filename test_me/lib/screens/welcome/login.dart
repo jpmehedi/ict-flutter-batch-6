4039b4bf17dde325d5844652fb10b5dc51e94362
Mehedi Hasan <mehedi@augnitive.com>
2022-01-01 16:09:02 +0600

    Write data and store image

--- test_me/lib/screens/welcome/login.dart
+++ test_me/lib/screens/welcome/login.dart
@@ -14,6 +14,7 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  
   bool isLoading = false;
   TextEditingController _emailEditingController = TextEditingController();
   TextEditingController _passwordEditingController = TextEditingController();
